493db74b0c8be95d19799ce0278e74dd576f13f6
curtca <curt.carpenter@live.com>
2020-12-02 21:45:55 -0800

    3.1

---
+++ 3.1/lib/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace lib
+{
+    public class Toboggan
+    {
+        public int CountTrees3R1D(string input) // 3 right, 1 down
+        {
+            string[] lines = input.Split("\r\n");
+            int width = lines[0].Length;
+            int height = lines.Length;
+            int cTrees = 0;
+            int x = 0;
+            for (int y = 1; y < height; y++)
+            {
+                x = (x + 3) % width;
+
+                if (lines[y][x] == '#')
+                    cTrees++;
+            }
+            return cTrees;
+        }
+    }
+}
