78b16e165fe15d974c43c431b110cb4b922baaea
curtca <curt.carpenter@live.com>
2020-12-06 09:05:41 -0800

    6.1

---
+++ 5.2/lib/Class1.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Seats
+{
+    string[] lines;
+    public Seats(string input)
+    {
+        lines = input.Split("\r\n");
+    }
+
+    public long Missing()
+    {
+        long max = 0;
+        long[] ids = new long[lines.Length];
+        for (int i = 0; i < lines.Length; i++)
+        {
+            ids[i] = ID(lines[i]);
+        }
+        Array.Sort(ids);
+
+        for (int i = 1; i < lines.Length; i++)
+        {
+            if (ids[i] - ids[i - 1] == 2)
+                return ids[i] - 1;
+        }
+
+        return -1;
+    }
+
+    private long ID(string v)
+    {
+        int maxrow = 127;
+        int minrow = 0;
+        int maxseat = 7;
+        int minseat = 0;
+
+        for (int i = 0; i < 7; i++)
+        {
+            int shrink = 1 << (6 - i);
+            if (v[i] == 'F')
+                maxrow -= shrink;
+            else
+                minrow += shrink;
+        }
+        for (int i = 0; i < 3; i++)
+        {
+            int shrink = 1 << (2 - i);
+            if (v[7 + i] == 'L')
+                maxseat -= shrink;
+            else
+                minseat += shrink;
+        }
+        return minrow * 8 + minseat;
+    }
+}
---
+++ 6.1/lib/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Customs
+{
+    string[] forms;
+    public Customs(string input)
+    {
+        forms = input.Split("\r\n\r\n");
+    }
+
+    public long SumOfQsAnswered()
+    {
+        long sum = 0;
+        long[] ids = new long[forms.Length];
+        for (int i = 0; i < forms.Length; i++)
+        {
+            string form = forms[i].Replace("\r\n", null);
+            var chars = new HashSet<char>();
+            for (int j = 0; j < form.Length; j++)
+                chars.Add(form[j]);
+
+            sum += chars.Count;
+        }
+
+        return sum;
+    }
+}
