2ea6a9b57d6c6a0da1ee2dcb23421b378a83ec51
curtca <curt.carpenter@live.com>
2020-12-08 21:54:30 -0800

    9.1

---
+++ 9.1/lib/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class XMAS
+{
+    long[] nums;
+    public XMAS(string input)
+    {
+        var lines = input.Split("\r\n");
+        nums = new long[lines.Length];
+        for (int i = 0; i < lines.Length; i++)
+        {
+            nums[i] = long.Parse(lines[i]);
+        }
+    }
+
+    public long FindInvalid(int preamble)
+    {
+        for (int i = preamble; i < nums.Length; i++) // candidate invalid num
+        {
+            bool valid = false;
+            for (int j = i - preamble; j < i - 1; j++)
+            {
+                for (int k = j + 1; k < i; k++)
+                {
+                    if (nums[j] + nums[k] == nums[i])
+                        valid = true;
+                }
+            }
+            if (!valid)
+                return nums[i];
+        }
+
+        return -1;
+    }
+}
