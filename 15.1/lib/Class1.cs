866741f0ae67de25335ad5486c5a27365c618e54
curtca <curt.carpenter@live.com>
2020-12-16 11:34:58 -0800

    15.1

---
+++ 15.1/lib/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Diagnostics;
+using System.Text;
+
+public class Memory
+{
+    List<long> nums = null;
+    public Memory(string input)
+    {
+        var strnums = input.Split(",");
+        nums = (from sn in strnums
+                    select long.Parse(sn)).ToList();
+    }
+
+    public long Nth(int n)
+    {
+        while (nums.Count < 2020)
+        {
+            int c = nums.Count;
+            int iprev = c - 2;
+            while (iprev >= 0 && nums[iprev] != nums[c - 1])
+                iprev--;
+
+            if (iprev == -1) // first in list
+                nums.Add(0);
+            else
+                nums.Add(c - iprev - 1);
+        }
+        return nums.Last();
+    }
+}
---
+++ 15.1/test/UnitTest1.cs
@@ -0,0 +1,40 @@
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace test
+{
+    public class UnitTest1
+    {
+        [Theory]
+        [MemberData(nameof(Data))]
+        public void Test(string input, long expected)
+        {
+            Memory m = new Memory(input);
+            long n = m.Nth(2020);
+            Assert.Equal(expected, n);
+        }
+
+        public static IEnumerable<object[]> Data =>
+            new List<object[]>
+            {
+                new object[] { sample1, 436 },
+                new object[] { sample2, 1 },
+                new object[] { sample3, 10 },
+                new object[] { sample4, 27 },
+                new object[] { sample5, 78 },
+                new object[] { sample6, 438 },
+                new object[] { sample7, 1836 },
+                new object[] { input, 0 },
+            };
+
+        static string sample1 = @"0,3,6";
+        static string sample2 = @"1,3,2";
+        static string sample3 = @"2,1,3";
+        static string sample4 = @"1,2,3";
+        static string sample5 = @"2,3,1";
+        static string sample6 = @"3,2,1";
+        static string sample7 = @"3,1,2";
+        static string input = @"1,12,0,20,8,16";
+    }
+}
