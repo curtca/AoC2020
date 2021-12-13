4c071a976abac4a0908ce92ac617b467490f6d00
curtca <curt.carpenter@live.com>
2021-12-12 16:23:36 -0800

    25

---
+++ 25.1/25.1/Class1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+public class Crypto
+{
+    long public1, public2;
+    public Crypto(string input)
+    {
+        var lines = input.Split(Environment.NewLine);
+        public1 = long.Parse(lines[0]);
+        public2 = long.Parse(lines[1]);
+    }
+
+    public long GetEncryptionKey()
+    {
+        long loops = 0;
+        long mod = 20201227;
+        long sub = 7;
+        long res = 1;
+        do
+        {
+            loops++;
+            res = (res * sub) % mod;
+        } while (res != public1);
+
+        sub = public2;
+        res = 1;
+        for (int i = 0; i < loops; i++)
+        {
+            res = (res * sub) % mod;
+        }
+        return res;
+    }
+}
---
+++ 25.1/25.1Tests/CryptoTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tests
+{
+    [TestClass()]
+    public class CryptoTests
+    {
+        [TestMethod()]
+        [DynamicData("TestData")]
+        public void CryptoTest(string input, long output)
+        {
+            var c = new Crypto(input);
+            long e = c.GetEncryptionKey();
+            Assert.AreEqual(output, e);
+        }
+
+        public static IEnumerable<object[]> TestData =>
+            new List<object[]>
+            {
+                new object[] { sample, 14897079 },
+                new object[] { input, 711945 },
+            };
+
+        static string sample =
+@"5764801
+17807724";
+
+        static string input =
+@"11239946
+10464955";
+    }
+}
